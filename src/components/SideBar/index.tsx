ee4ce7ce8baac868ee1486a6eef32fdc7e6fb4e0
Guilherme-Aguiar <gui9627oli@gmail.com>
2020-08-29 16:25:46 -0300

    Created sidebar component
    Signed-off-by: Guilherme-Aguiar <gui9627oli@gmail.com>
    Co-authored-by: Lucas Siqueira <lucassiqzro@gmail.com>

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import StickyBox from 'react-sticky-box';
+
+import List from '../List';
+import FollowSuggestion from '../FollowSuggestion';
+import News from '../News';
+
+import {
+  Container,
+  SearchWrapper,
+  SearchInput,
+  SearchIcon,
+  Body,
+} from './styles';
+
+const SideBar: React.FC = () => {
+  return (
+    <Container>
+      <SearchWrapper>
+        <SearchInput placeholder="Buscar no Twitter" />
+        <SearchIcon />
+      </SearchWrapper>
+
+      <StickyBox>
+        <Body>
+          {/* Aplicar grafo aqui */}
+          <List
+            title="Talvez você curta"
+            elements={[
+              <FollowSuggestion name="Lucas Siqueira" nickname="@lucassiqz" />,
+              <FollowSuggestion name="Lucas Siqueira" nickname="@lucassiqz" />,
+              <FollowSuggestion name="Lucas Siqueira" nickname="@lucassiqz" />,
+            ]}
+          />
+          <List
+            title="O que está acontecendo"
+            elements={[<News />, <News />, <News />]}
+          />
+        </Body>
+      </StickyBox>
+    </Container>
+  );
+};
+
+export default SideBar;
